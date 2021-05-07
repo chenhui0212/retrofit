b5dd16ca8df18e18e4b3244a6299c982176eaddd
chenhui0212 <chenhui19900212@gmail.com>
2021-05-07 10:45:24 +0800

    Fix compile warning

--- retrofit/src/main/java/retrofit2/RequestFactory.java
+++ retrofit/src/main/java/retrofit2/RequestFactory.java
@@ -346,6 +346,7 @@ final class RequestFactory {
               return null;
             }
           } catch (NoClassDefFoundError ignored) {
+              // Ignored
           }
         }
         throw parameterError(method, p, "No Retrofit annotation found.");
